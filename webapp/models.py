a91dd11222b84a2e4ec9e8fb7ddc428d9fd0d104
MarwanWalid2 <18085@stemoegypt.edu.eg>
2023-12-09 14:27:29 -0500

    switched to pymongo and finsished auth

--- webapp/models.py
+++ webapp/models.py
@@ -1,16 +1,6 @@
-from app import db
-from flask_login import UserMixin
+from pymongo import MongoClient
+from flask_login import LoginManager
+from bson.objectid import ObjectId
 
-class User(db.Document, UserMixin):
-    username = db.StringField(unique=True, required=True)
-    email = db.StringField(unique=True, required=True)
-    password_hash = db.StringField(required=True)
-
-class Transaction(db.Document):
-    user_id = db.ReferenceField(User)
-    type = db.StringField(required=True)  # 'income' or 'expense'
-    amount = db.DecimalField(required=True)
-    date = db.DateTimeField(required=True)
-    category = db.StringField()
-    notes = db.StringField()
-
+client = MongoClient('mongodb://localhost:27017/')
+db = client['bank']
