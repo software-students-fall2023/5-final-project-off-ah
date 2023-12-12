fd92cac9a88ec8713a5419f6ef90f182863e00c5
xinrantaoangela <xinran.tao2001@gmail.com>
2023-12-12 17:04:39 -0500

    edit the test code, the test code for models is working

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,29 +1,44 @@
 import unittest
+from flask import Flask
+from webapp.app import app
 from webapp.forms import LoginForm, RegisterForm, TransactionForm
 
-class TestForms(unittest.TestCase):
+class FormTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
     def test_login_form(self):
-        form = LoginForm(data={'username': 'testuser', 'password': 'testpass'})
-        self.assertTrue(form.validate())
+        with app.test_request_context():
+            form = LoginForm(data={'username': 'testuser', 'password': 'testpass'})
+            if not form.validate():
+                print("Login Form Errors:", form.errors)
+            self.assertTrue(form.validate(), msg="Login form validation failed")
 
     def test_register_form(self):
-        form = RegisterForm(data={
-            'username': 'testuser',
-            'email': 'test@example.com',
-            'password': 'testpass',
-            'confirm_password': 'testpass'
-        })
-        self.assertTrue(form.validate())
+        with app.test_request_context():
+            form = RegisterForm(data={
+                'username': 'testuser',
+                'email': 'test@example.com',
+                'password': 'testpass',
+                'confirm_password': 'testpass'
+            })
+            if not form.validate():
+                print("Register Form Errors:", form.errors)
+            self.assertTrue(form.validate(), msg="Register form validation failed")
 
     def test_transaction_form(self):
-        form = TransactionForm(data={
-            'description': 'test transaction',
-            'amount': 100.0,
-            'date': '2023-01-01',
-            'category': 'Test Category',
-            'notes': 'Test Note'
-        })
-        self.assertTrue(form.validate())
+        with app.test_request_context():
+            form = TransactionForm(data={
+                'description': 'Test Transaction',
+                'amount': 100.00,
+                'date': '2023-01-01',
+                'category': 'Test Category',
+                'notes': 'Test Notes'
+            })
+            if not form.validate():
+                print("Transaction Form Errors:", form.errors)
+            self.assertTrue(form.validate(), msg="Transaction form validation failed")
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,21 @@
 import unittest
-from webapp.models import db, MONGO_URI
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+DB_NAME = os.getenv('DB_NAME', 'bank')
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
 
 class TestModels(unittest.TestCase):
     def test_db_connection(self):
         self.assertIsNotNone(db)
-        self.assertEqual(db.name, 'bank')
-        self.assertIn('bank', MONGO_URI)
+        self.assertEqual(db.name, DB_NAME)
+        self.assertEqual(DB_NAME, 'bank')
 
 if __name__ == '__main__':
     unittest.main()
